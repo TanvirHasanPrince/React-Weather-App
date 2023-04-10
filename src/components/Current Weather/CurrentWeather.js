669fa1244920ce6ac95ac678ff4dfd6e69b80d9a
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-04-10 06:27:40 +0600

    weather componenet fill up

--- src/components/Current Weather/CurrentWeather.js
+++ src/components/Current Weather/CurrentWeather.js
@@ -4,13 +4,38 @@ import './Current_Weather.css';
 
 const CurrentWeather = () => {
  return (
-  <div className="weather">
-   <div className="top">
-    <p className='city'>Dhaka</p>
-    <p className='weather description'>Sunny</p>
+   <div className="weather">
+     <div className="top">
+       <p className="city">Dhaka</p>
+       <p className="weather description">Sunny</p>
+       <img src="icons/01d.png" className="weather-icon" alt="" />
+     </div>
+     <div className="bottom">
+       <p classname="temperature"> 18°c </p>
+       <div className="details">
+         <div className="parameter-row">
+           <span className="parameter-label">Deatils</span>
+         </div>
+         <div className="parameter-row">
+           <span className="parameter-label">Feels like</span>
+           <span className="parameter-value">21°C</span>
+         </div>
+         <div className="parameter-row">
+           <span className="parameter-label">Wind</span>
+           <span className="parameter-value">2 m/s</span>
+         </div>
+         <div className="parameter-row">
+           <span className="parameter-label">Humidity</span>
+           <span className="parameter-value">15%</span>
+         </div>
+
+         <div className="parameter-row">
+           <span className="parameter-label">Pressure</span>
+           <span className="parameter-value">15 hPa</span>
+         </div>
+       </div>
+     </div>
    </div>
-   <img src="icons/01d.png" className = 'weather-icon'alt="" />
-  </div>
  );
 };
 
