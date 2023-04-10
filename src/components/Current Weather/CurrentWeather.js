1e2cf422e383834338204d51ff6ae4f19ba76f2a
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2023-04-10 06:33:21 +0600

    weather styling

--- src/components/Current Weather/CurrentWeather.js
+++ src/components/Current Weather/CurrentWeather.js
@@ -6,15 +6,17 @@ const CurrentWeather = () => {
  return (
    <div className="weather">
      <div className="top">
-       <p className="city">Dhaka</p>
-       <p className="weather description">Sunny</p>
-       <img src="icons/01d.png" className="weather-icon" alt="" />
+       <div>
+         <p className="city">Dhaka</p>
+         <p className="weather-description">Sunny</p>
+       </div>
+       <img alt="" className="weather-icon" src="" />
      </div>
      <div className="bottom">
-       <p classname="temperature"> 18°c </p>
+       <p className="temperature">12°C</p>
        <div className="details">
          <div className="parameter-row">
-           <span className="parameter-label">Deatils</span>
+           <span className="parameter-label">Details</span>
          </div>
          <div className="parameter-row">
            <span className="parameter-label">Feels like</span>
@@ -22,16 +24,15 @@ const CurrentWeather = () => {
          </div>
          <div className="parameter-row">
            <span className="parameter-label">Wind</span>
-           <span className="parameter-value">2 m/s</span>
+           <span className="parameter-value">16 m/s</span>
          </div>
          <div className="parameter-row">
            <span className="parameter-label">Humidity</span>
            <span className="parameter-value">15%</span>
          </div>
-
          <div className="parameter-row">
            <span className="parameter-label">Pressure</span>
-           <span className="parameter-value">15 hPa</span>
+           <span className="parameter-value">12 hPa</span>
          </div>
        </div>
      </div>
